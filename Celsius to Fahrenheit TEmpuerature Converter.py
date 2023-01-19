bd4e562255268ae10eea5a111c7f660b2d5bddfb
KatieMeuer <122391236+KatieMeuer@users.noreply.github.com>
2023-01-19 14:00:02 -0600

    Add files via upload

---
+++ Celsius to Fahrenheit TEmpuerature Converter.py
@@ -0,0 +1,13 @@
+#Katie Meuer, Celsuis to Fahrenheit, 1/19/2023
+#Create variable for user to input C degrees
+#write equation
+#Print
+
+#Create variable for user to input C degrees
+Celsius = float(input('Type degrees in Celsius here: '))
+
+#write equation
+Farenheit = 9/5 *Celsius + 32
+
+#Print
+print(f'In degrees Celsius that is {Farenheit:,.2f}.')
