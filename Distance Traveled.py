ed099d1318a9a215bde0284a2396971fea9bd122
KatieMeuer <122391236+KatieMeuer@users.noreply.github.com>
2023-01-19 13:18:52 -0600

    Add files via upload

---
+++ Distance Traveled.py
@@ -0,0 +1,17 @@
+#Katie Meuer, Distance Traveled, 1/19/2023
+#Create variables for time and speed
+#Create equation with all three
+
+#Variables
+Speed = 70
+Time = 6
+#Equation
+Distance = Speed * Time
+#Print each distance depending on the time
+print(f'The distance traveled after {Time} hours is {Distance} miles.')
+Time2 = 10
+Distance = Speed * Time2
+print(f'The distance traveled after {Time2} hours is {Distance} miles.')
+Time3 = 15
+Distance = Speed * Time3
+print(f'The distance traveled after {Time3} hours is {Distance} miles.')
